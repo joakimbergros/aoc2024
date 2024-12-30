159c81adac2ee512eed418aeff8884928b222b3d
Joakim Bergros <joakim.bergros@svedala.se>
2024-12-30 14:00:53 +0100

    I think I'm overcomplicating it...

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -1,45 +1,116 @@
-use std::{collections::HashMap, iter::{self, Filter}};
+use std::{
+    collections::HashMap,
+    iter::{self, Filter},
+};
 
+use crate::Direction::{East, North, NorthEast, NorthWest, South, SouthEast, SouthWest, West};
 use itertools::Itertools;
 
 advent_of_code::solution!(4);
 
 const EXPECTED_WORDS: [char; 4] = ['X', 'M', 'A', 'S'];
+const WANTED_CHARS: [char; 3] = ['M', 'A', 'S'];
 const NEIGHBOUR_MAP: [(isize, isize); 9] = [
-    (-1, -1), (-1, 0), (-1, 1),
-    (0, -1), (0, 0), (0, 1),
-    (1, -1), (1, 0), (1, 1),
+    (-1, -1),
+    (-1, 0),
+    (-1, 1),
+    (0, -1),
+    (0, 0),
+    (0, 1),
+    (1, -1),
+    (1, 0),
+    (1, 1),
 ];
 
-type Coordinate = (usize, usize);
+const NEIGHBOUR_MAP2: [Direction; 8] = [
+    NorthWest(-1, -1),
+    North(-1, 0),
+    NorthEast(-1, 1),
+    West(0, -1),
+    East(0, 1),
+    SouthWest(1, -1),
+    South(1, 0),
+    SouthEast(1, 1),
+];
+
+enum Direction {
+    NorthWest(isize, isize),
+    North(isize, isize),
+    NorthEast(isize, isize),
+    West(isize, isize),
+    East(isize, isize),
+    SouthWest(isize, isize),
+    South(isize, isize),
+    SouthEast(isize, isize),
+}
+
+impl Direction {
+    fn from_coordinate(coord: Coordinate) -> Self {}
+    fn values(&self) -> (&isize, &isize) {
+        match self {
+            NorthWest(x, y)
+            | North(x, y)
+            | NorthEast(x, y)
+            | West(x, y)
+            | East(x, y)
+            | SouthWest(x, y)
+            | South(x, y)
+            | SouthEast(x, y) => (x, y),
+        }
+    }
+
+    fn next(&self) -> Self {
+        match self {
+            NorthWest(x, y) => Direction::NorthWest(x - 1, y - 1),
+            North(x, y) => Direction::North(x - 1, y.clone()),
+            NorthEast(x, y) => Direction::NorthEast(x - 1, y + 1),
+            West(x, y) => Direction::West(x.clone(), y - 1),
+            East(x, y) => Direction::East(x.clone(), y + 1),
+            SouthWest(x, y) => Direction::SouthWest(x + 1, y - 1),
+            South(x, y) => Direction::South(x - 1, y.clone()),
+            SouthEast(x, y) => Direction::SouthEast(x - 1, y + 1),
+        }
+    }
+}
+
+type Coordinate = (isize, isize);
 type Map = HashMap<Coordinate, char>;
 
 trait Mappable {
-    fn scan(&self, coordinate: &Coordinate, expected: char) -> u32;
+    fn scan(&self, coordinate: &Coordinate, index: usize) -> u32;
 }
 
 impl Mappable for Map {
-    fn scan(&self, coordinate: &Coordinate, expected: char) -> u32 {
+    fn scan(&self, coordinate: &Coordinate, index: usize) -> u32 {
+        if EXPECTED_WORDS.len() == index {
+            return 0;
+        }
+
+        dbg!(&coordinate, EXPECTED_WORDS[index]);
         let mut appearances = 0;
 
         for (x, y) in NEIGHBOUR_MAP {
-            let fx=  coordinate.0 as isize + x;
-            let fy=  coordinate.1 as isize + y;
+            let fx = coordinate.0 as isize + x;
+            let fy = coordinate.1 as isize + y;
 
             if fx.is_negative() || fy.is_negative() {
                 continue;
             }
 
-            let next_coordinate = (fx as usize, fy as usize);
+            let next_coordinate = (fx, fy);
 
             let letters = match self.get(&next_coordinate) {
-                Some(v) if *v == expected => self.scan(&next_coordinate, expected),
-                _ => continue,
+                Some(v) if *v == EXPECTED_WORDS[index] => {
+                    dbg!(v);
+                    self.scan(&next_coordinate, index + 1)
+                }
+                c => {
+                    dbg!(c);
+                    continue;
+                }
             };
 
-            if letters != 3 {
-                continue;
-            }
+            dbg!(letters);
 
             appearances += 1;
         }
@@ -53,23 +124,75 @@ fn parse(input: &str) -> HashMap<Coordinate, char> {
 
     for (li, lv) in input.lines().enumerate() {
         for (ci, cv) in lv.chars().enumerate() {
-            map.insert((li, ci), cv);
+            map.insert((li as isize, ci as isize), cv);
         }
     }
-    
+
     map
 }
 
 pub fn part_one(input: &str) -> Option<u32> {
     let map = parse(input);
-    let mut total = 0;
+    //let mut total = 0;
 
-    for (coordinate, c) in &map {
+    let result: u32 = map
+        .iter()
+        .filter(|(_, c)| **c == 'X')
+        .map(|coord| {
+            for direction in &NEIGHBOUR_MAP2 {
+                direction.next()
+            }
+
+            for wanted in &WANTED_CHARS {}
+
+            let mut start_char_index = 1;
+            let mut total_words = 0;
+
+            for dir in &NEIGHBOUR_MAP2 {
+                let (dx, dy) = dir.values();
+                let ((cx, cy), _) = coord;
+                let fx = dx + cx;
+                let fy = dy + cy;
+
+                match map.get(&(fx, fy)) {
+                    Some(letter) if *letter == EXPECTED_WORDS[start_char_index] => true,
+                    _ => {
+                        continue;
+                    }
+                };
+
+                total_words += 1;
+
+                /*let next_coordinate = (fx as usize, fy as usize);
+
+                let letters = match self.get(&next_coordinate) {
+                    Some(v) if *v == EXPECTED_WORDS[index] => {
+                        dbg!(v);
+                        self.scan(&next_coordinate, index + 1)
+                    },
+                    c => {
+                        dbg!(c);
+                        continue
+                    },
+                };
+
+                dbg!(letters);
+
+                appearances += 1;*/
+            }
+
+            total_words
+        })
+        .sum();
+
+    dbg!(result);
+
+    /*for (coordinate, c) in &map {
         if *c != 'X' {
             continue;
         }
 
-        total += map.scan(coordinate, 'M');
+        total += map.scan(coordinate, 0);
 
         /* for (x, y) in NEIGHBOUR_MAP {
             let fx=  coordinate.0 as isize + x;
@@ -84,11 +207,11 @@ pub fn part_one(input: &str) -> Option<u32> {
                 _ => continue,
             };
 
-            
-        } */
-    }
 
-    Some(total)
+        } */
+    }*/
+
+    Some(123)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
--- src/bin/05.rs
+++ src/bin/05.rs
@@ -1,27 +1,32 @@
-use nom::{bytes::complete::tag, character::complete::{digit1, line_ending, newline}, combinator::map_res, error::Error, multi::separated_list1, sequence::{preceded, separated_pair, terminated}, IResult};
+use nom::{
+    bytes::complete::tag,
+    character::complete::{digit1, line_ending},
+    combinator::map_res,
+    multi::{many1, separated_list1},
+    sequence::{separated_pair, terminated},
+    IResult,
+};
 
 advent_of_code::solution!(5);
 
-fn parse_updates(input: &str) -> IResult<&str, Vec<u32>> {
-    terminated(
-    preceded(
-        newline, 
-        separated_list1(
-            tag(","),
-            map_res(digit1, |s: &str| s.parse::<u32>())
-            )
-        ),
-    newline
+fn parse_updates(input: &str) -> IResult<&str, Vec<Vec<u32>>> {
+    separated_list1(
+        line_ending,
+        separated_list1(tag(","), map_res(digit1, |s: &str| s.parse::<u32>())),
     )(input)
 }
 
 fn parse_ordering(input: &str) -> IResult<&str, Vec<(u32, u32)>> {
-    separated_list1(
-        line_ending,
-        separated_pair(
-        map_res(digit1, |s: &str| s.parse::<u32>()),
-        tag("|"),
-        map_res(digit1, |s: &str| s.parse::<u32>()))
+    terminated(
+        separated_list1(
+            line_ending,
+            separated_pair(
+                map_res(digit1, |s: &str| s.parse::<u32>()),
+                tag("|"),
+                map_res(digit1, |s: &str| s.parse::<u32>()),
+            ),
+        ),
+        many1(line_ending),
     )(input)
 }
 
@@ -30,7 +35,7 @@ pub fn part_one(input: &str) -> Option<u32> {
         panic!("unable to parse orderings");
     };
 
-    dbg!(rest);
+    dbg!(&rest);
 
     let Ok((rest, updates)) = parse_updates(rest) else {
         panic!("unable to parse updates");
@@ -52,7 +57,7 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(17));
     }
 
     #[test]
